b672340275f8f5aaaf264c41916ad2ec9d4affa3
SOumitra Maity <maitychottu03225@gmail.com>
2026-06-02 23:56:13 +0530

    search route updated

--- Nodejs/controller/searchController.js
+++ Nodejs/controller/searchController.js
@@ -1,23 +1,58 @@
-const Search=require('../models/Search')
+const Search = require('../models/Search')
 
-exports.getSearch=async(req, res)=>{
-    try{
-        const search=req.query.search || ""
-        const products= await Search.find({
-            name:{
-                $regex: search,
-                $options: "i"
+exports.getSearch = async (req, res) => {
+    try {
+        const search = req.query.search || ""
+        const products = await Search.find()
+
+        const searchResult = search.toLowerCase()
+
+        if (!search) {
+            return res.status(200).json({
+                success: true,
+                count: products.length,
+                products
+            })
+        }
+
+        const rankedSearch=products.map(product=>{
+            let score=0
+            const name=(product.name||"").toLowerCase()
+
+            if(name===searchResult){
+                score+=100
+            }
+            else if(name.startsWith(searchResult)){
+                score += 80
+            }
+            else if(name.includes(searchResult)){
+                score+=60
+            }
+            else{
+                const matchCount=searchResult
+                .split("")
+                .filter(char=>name.includes(char)).length
+
+               score=matchCount>3? 30 :0
+            }
+            return {
+                ...product.toObject(),
+                score
             }
         })
-        res.status(200).json({
-            success:true,
-            count:products.length,
-            products
+        .filter(product=>product.score>0)
+        .sort((a,b)=>b.score-a.score)
+
+        return res.status(200).json({
+            success: true,
+            count: rankedSearch.length,
+            products: rankedSearch
         })
+
     }
-    catch(error){
+    catch (error) {
         res.status(500).json({
-            success: flase,
+            success: false,
             message: error.message
         })
     }
