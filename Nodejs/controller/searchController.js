f6df139d6632e433ad8d46b430a0099ef2ad1b28
SOumitra Maity <maitychottu03225@gmail.com>
2026-06-03 23:05:46 +0530

    update search and add create product controller

--- Nodejs/controller/searchController.js
+++ Nodejs/controller/searchController.js
@@ -2,10 +2,19 @@ const Search = require('../models/Search')
 
 exports.getSearch = async (req, res) => {
     try {
-        const search = req.query.search || ""
-        const products = await Search.find()
+        const {search="", category, brand} = req.query
+        let query={}
 
+         if(category){
+            query.category=category
+        }
+        if(brand){
+            query.brand=brand
+        }
+        const products = await Search.find(query)
+        
         const searchResult = search.toLowerCase()
+       
 
         if (!search) {
             return res.status(200).json({
@@ -53,6 +62,33 @@ exports.getSearch = async (req, res) => {
     catch (error) {
         res.status(500).json({
             success: false,
+            message: error.message
+        })
+    }
+}
+exports.createProduct=async (req, res)=>{
+    
+    try{
+        const {name, category, brand, price}=req.body;
+        const existingSearch=await Search.findOne({name})
+        if(existingSearch){
+            return res.status(400).json({
+                message: "user already exist"
+            })
+        }
+        const products= await Search.create({
+            name:name, category:category, brand:brand, price:price
+        })
+
+        return res.status(201).json({
+            success:true,
+            products,
+            message: "product created successfully"
+        })
+    }
+    catch(error){
+        return res.status(500).json({
+            success:false,
             message: error.message
         })
     }
